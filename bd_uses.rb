cc431bab3a656e9383b9add68443767a49e90455
AIM <abzalov.im@gmail.com>
2016-03-22 03:43:53 +0300

    use db

--- bd_uses.rb
+++ bd_uses.rb
@@ -5,6 +5,33 @@ require './connect_db.rb'
 
 DB.create_table?(:cashes)do
 	primary_key :id
-	String :names
-	Integer :percent, :default => 10
+	String :name
+	Integer :cashe_percent, :default => 10
+end
+
+DB.create_table?(:cards)do
+	primary_key :id
+	String :barcode
+	Integer :card_percent, :default => 10
+  unique :barcode
+end
+
+DB.create_table?(:bonuses)do
+  foreign_key :card_id
+  foreign_key :cashe_id
+  String :check_id
+	Float :value
+end
+
+class Cashe < Sequel::Model
+  one_to_many :bonuses
+end
+
+class Card < Sequel::Model
+  one_to_many :bonuses
+end
+
+class Bonus < Sequel::Model
+  many_to_one :cards
+  many_to_one :cashes  
 end
--- main.rb
+++ main.rb
@@ -4,5 +4,19 @@ require './bd_uses.rb'
 require 'haml'
 
 get '/' do
-  json :foo => 'bar'
+  @cahes = Cashe.all.order(:name)
+  haml :index
+end
+
+get '/cards/:cashe/:barcode' do
+  # card balanse
+  @response = {}
+  @response[:cashe_percent]=Cashe.select(:percent).where(:id=>params[:cashe])
+  @card_value = Cards.left_join(:bonuses, :card_id=>:id).where(:barcode=>params[:barcode]).select_group(:card_percent).select_append{sum(:value).as(total)}
+  @response[:card_percent]=@card_value[:card_percent]
+  @response[:bonuses]=@card_value[:total]
+end
+
+post '/cards/:barcode/:sum_out/:sum_in' do
+  
 end
