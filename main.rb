7ac25b1a34a201a542e945c5755578ab6ce7c722
Salimbek <abzalov.im@gmail.com>
2016-03-22 17:19:22 +0300

    first working

--- main.rb
+++ main.rb
@@ -2,21 +2,25 @@ require 'sinatra'
 require 'sinatra/json'
 require './bd_uses.rb'
 require 'haml'
+require "sinatra/reloader" if development?
 
 get '/' do
-  @cahes = Cashe.all.order(:name)
+  @cashes = Cashe.order(:name).all
   haml :index
 end
 
 get '/cards/:cashe/:barcode' do
   # card balanse
-  @response = {}
-  @response[:cashe_percent]=Cashe.select(:percent).where(:id=>params[:cashe])
-  @card_value = Cards.left_join(:bonuses, :card_id=>:id).where(:barcode=>params[:barcode]).select_group(:card_percent).select_append{sum(:value).as(total)}
-  @response[:card_percent]=@card_value[:card_percent]
-  @response[:bonuses]=@card_value[:total]
+  DB[:cashes].insert_ignore.insert(:id=>params[:cashe], :name=>"Касса №#{params[:cashe]}")
+  DB[:cards].insert_ignore.insert(:barcode=>params[:barcode])
+  @resp = {}
+  @resp[:cashe_percent]=DB[:cashes].where(:id=>params[:cashe]).get(:cashe_percent)
+  @resp[:card_percent]=DB[:cards].where(:barcode=>params[:barcode]).get(:card_percent)
+  @card_value = DB[:cards].where(:barcode=>params[:barcode]).left_join(:bonuses, :card_id=>:id).sum(:value)
+  @resp[:bonuses]= @card_value.nil? ? 0 : @card_value
+  json @resp, :layout => nil
 end
 
-post '/cards/:barcode/:sum_out/:sum_in' do
+post '/cards/:cashe/:chec_id/:barcode/:sum_out/:sum_in' do
   
 end
