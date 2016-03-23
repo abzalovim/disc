e673c35e80758418a0d462248d5ccd3a20824d8f
Salimbek <abzalov.im@gmail.com>
2016-03-23 19:58:51 +0300

    frontol script

---
+++ frontol_script.js
@@ -0,0 +1,79 @@
+function getXmlHttp(){
+    try {
+        return new ActiveXObject("Msxml2.XMLHTTP");
+    } catch (e) {
+        try {
+            return new ActiveXObject("Microsoft.XMLHTTP");
+        } catch (ee) {
+        }
+    }
+    if (typeof XMLHttpRequest!='undefined') {
+        return new XMLHttpRequest();
+    }
+}
+
+function reRun(RO) {
+    return RO.UserValues.Get('Ready');
+}
+
+function getUrl(AO, RO, POS, CO, UP) {
+    var xmlhttp = getXmlHttp();
+    url="http://192.168.0.8:4567";
+    // IE êýøèðóåò XMLHttpRequest çàïðîñû, òàê ÷òî äîáàâëÿåì ñëó÷àéíûé ïàðàìåòð ê URL
+    // (õîòÿ ìîæíî îáîéòèñü ïðàâèëüíûìè çàãîëîâêàìè íà ñåðâåðå)
+
+    RO.UserValues.Set('DiscountValue',0);
+    pos_id = RO.NShop.toString();
+    doc_no = RO.ReceiptNo.toString();
+    barcode= '0130019979780';
+    payment= RO.SummWD.toString();
+    sNewBonus = '70';
+
+
+    RO.UserValues.Set('Ready',0);
+    xmlhttp.open("GET", url+'/cards/'+pos_id+'/'+barcode+'/'+Math.random());
+    xmlhttp.send(null);
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            RO.UserValues.Set('Ready',1);
+        }
+    }
+
+    counter=0;
+    AO.ShowMessage('Ïðîâåðÿåì áàëàíñ êàðòû',Icon.Exclamation,2);
+    if (xmlhttp.readyState == 4) {
+        var data = eval('('+xmlhttp.responseText+')');
+        sUsedBonus = data['bonuses'].toString();
+        RO.UserValues.Set('DiscountValue',data['bonuses']);
+        AO.ShowMessage('Bonuses: '+sUsedBonus);
+        params = "/cards/"+pos_id+"/"+doc_no+"/"+barcode+'/'+payment+'/'+sUsedBonus+'/'+sNewBonus;
+        xmlhttp.open("GET", url+params);
+        xmlhttp.send(null);
+    }
+    else
+    {
+        AO.ShowMessage('Íåò ñâÿçè ñ ñåðâåðîì!');
+    }
+}
+
+
+function BeforeAct(AO, RO, E, O, CO)
+{
+}
+
+function AfterAct(AO, RO, E, O, CO)
+{
+
+}
+
+function FuncAct(AO, RO, CO)
+{
+}
+
+function q(AO, RO, POS, CO)
+{
+    card_number = RO.UserValues.Get("card_number");
+    summ = parseInt(card_number);
+    result = summ;
+    return result
+}
