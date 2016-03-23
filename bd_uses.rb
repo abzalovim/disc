522b7ad769c57929017f2fbcc519a92d6de978ad
Salimbek <abzalov.im@gmail.com>
2016-03-23 19:56:53 +0300

    sequel models

--- bd_uses.rb
+++ bd_uses.rb
@@ -3,35 +3,54 @@ require 'sequel'
 
 require './connect_db.rb'
 
-DB.create_table?(:cashes)do
+DB.create_table?(:cashes) do
 	primary_key :id
 	String :name
 	Integer :cashe_percent, :default => 10
 end
 
-DB.create_table?(:cards)do
+DB.create_table?(:cards) do
 	primary_key :id
 	String :barcode
 	Integer :card_percent, :default => 10
   unique :barcode
 end
 
-DB.create_table?(:bonuses)do
+DB.create_table?(:bonuses) do
   foreign_key :card_id
   foreign_key :cashe_id
   String :check_id
-	Float :value
+  Float :value
+  DateTime :updated_at
+  DateTime :created_at
 end
 
-class Cashe < Sequel::Model
+DB.create_table?(:payments) do
+  foreign_key :card_id
+  foreign_key :cashe_id
+  String :check_id
+  Float :value
+  DateTime :updated_at
+  DateTime :created_at
+end
+
+class Cashe < Sequel::Model(:cashes)
   one_to_many :bonuses
 end
 
-class Card < Sequel::Model
+class Card < Sequel::Model(:cards)
   one_to_many :bonuses
 end
 
-class Bonus < Sequel::Model
+class Bonus < Sequel::Model(:bonuses)
+  plugin :timestamps
   many_to_one :cards
-  many_to_one :cashes  
+  many_to_one :cashes
 end
+
+class Payment < Sequel::Model(:payments)
+  plugin :timestamps
+  many_to_one :cards
+  many_to_one :cashes
+end
+
--- main.rb
+++ main.rb
@@ -9,7 +9,7 @@ get '/' do
   haml :index
 end
 
-get '/cards/:cashe/:barcode' do
+get '/cards/:cashe/:barcode/:pin' do
   # card balanse
   DB[:cashes].insert_ignore.insert(:id=>params[:cashe], :name=>"Касса №#{params[:cashe]}")
   DB[:cards].insert_ignore.insert(:barcode=>params[:barcode])
@@ -17,10 +17,23 @@ get '/cards/:cashe/:barcode' do
   @resp[:cashe_percent]=DB[:cashes].where(:id=>params[:cashe]).get(:cashe_percent)
   @resp[:card_percent]=DB[:cards].where(:barcode=>params[:barcode]).get(:card_percent)
   @card_value = DB[:cards].where(:barcode=>params[:barcode]).left_join(:bonuses, :card_id=>:id).sum(:value)
-  @resp[:bonuses]= @card_value.nil? ? 0 : @card_value
+  @resp[:bonuses] = @card_value.nil? ? 0 : @card_value
   json @resp, :layout => nil
 end
 
-post '/cards/:cashe/:chec_id/:barcode/:sum_out/:sum_in' do
-  
+get '/cards/:cashe/:check_id/:barcode/:payment/:sum_out/:sum_in' do
+  card_id = DB[:cards].where(:barcode=>params[:barcode]).get(:id)
+  str_sum = params[:sum_out]
+  value = 0-str_sum.to_f
+  Bonus.create({:card_id => card_id, :cashe_id => params[:cashe], :check_id => params[:check_id], :value => value})
+
+  p "#{params[:check_id]} : #{params[:barcode]} : #{params[:payment]} : #{params[:sum_out]} : #{params[:sum_in]}"
+  sum_in = params[:sum_in]
+  value = sum_in.to_f
+  Bonus.create({:card_id => card_id, :cashe_id => params[:cashe], :check_id => params[:check_id], :value => value})
+
+  payment = params[:payment]
+  value = payment.to_f
+  Payment.create({:card_id => card_id, :cashe_id => params[:cashe], :check_id => params[:check_id], :value => value})
+
 end
