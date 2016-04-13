6c64d7f93ee4429e1cedf1a88d0e71b43bf00147
Salimbek <abzalov.im@gmail.com>
2016-04-13 16:02:42 +0300

    fix N2

--- main.rb
+++ main.rb
@@ -2,25 +2,33 @@ require 'sinatra'
 require 'sinatra/json'
 require './bd_uses.rb'
 require 'haml'
-# require "sinatra/reloader" if development?
+require 'xmlsimple'
+require 'sinatra/respond_with'
+require "sinatra/reloader" if development?
 
 get '/' do
   @cashes = Cashe.order(:name).all
   haml :index
 end
 
-get '/cards/:cashe/:barcode/:pin', :provides => [:json, :xml] do
+get '/cards/:cashe/:barcode/:pin/:resp' do
   # card balanse
   DB[:cashes].insert(:id=>params[:cashe], :name=>"Касса №#{params[:cashe]}") if Cashe[params[:cashe]].nil?
   DB[:cards].insert_conflict.insert(:barcode=>params[:barcode]) if Card[{:barcode=>params[:barcode]}].nil?
   @resp = {}
-  @resp[:cashe_percent]=DB[:cashes].where(:id=>params[:cashe]).get(:cashe_percent)
-  @resp[:card_percent]=DB[:cards].where(:barcode=>params[:barcode]).get(:card_percent)
+  @resp["cashe_percent"]=DB[:cashes].where(:id=>params[:cashe]).get(:cashe_percent)
+  @resp["card_percent"]=DB[:cards].where(:barcode=>params[:barcode]).get(:card_percent)
   @card_value = DB[:cards].where(:barcode=>params[:barcode]).left_join(:bonuses, :card_id=>:id).sum(:value)
-  @resp[:bonuses] = @card_value.nil? ? 0 : @card_value
-  respond_to do |f|
-    f.json{ json @resp, :layout => nil }
-    f.xml{ XmlSimple.xml_out(@resp, {:keeproot => true, :noescape => true}) }
+  @resp["bonuses"] = @card_value.nil? ? 0 : @card_value
+  st_resp = params[:resp]
+  if st_resp == 'xml'
+    r = '<?xml version="1.0" encoding="utf-8"?>'+"\n"
+    r+=XmlSimple.xml_out({"id"=>"0", "keys"=>@resp}, {:keeproot => true, :noescape => true, 'AttrPrefix' => true })
+    p r
+  elsif st_resp == 'json'
+    json @resp, :layout => nil
+  else
+    p 'Error in query'
   end
 end
 
--- script.js
+++ script.js
@@ -10,3 +10,20 @@ function getDiscount(AO, RO, POS, CO, UV)
     //AO.ShowMessage('ret:'+ret);
     return Math.min(v1,v2);
 }
+
+function q(AO, RO, POS, CO, UV)
+{
+var xml = new ActiveXObject("Microsoft.XMLDOM");
+xml.async = false;
+r = xml.load("http://10.0.2.2:4567/cards/1/0130019979780/19/xml");
+//AO.ShowMessage("Start");
+if (r) {
+x = xml.getElementsByTagName("keys")[0];
+for (var i=0; i<x.childNodes.length; i++) {
+    var child = x.childNodes[i];
+    AO.ShowMessage(child.tagName+":"+child.text);
+}
+
+xml = false;
+}
+}
